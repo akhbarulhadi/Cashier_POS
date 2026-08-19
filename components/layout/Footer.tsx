2cf16fa8fda70bb6f9d50b59ce7901986aba21e9
akhbarulhadi <akhbarulhadi44@gmail.com>
2026-08-20 00:50:16 +0700

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="mt-auto py-4 text-center text-sm text-muted-foreground border-t">
-      <p>Made by 💖Akhbarul Hadi (2026)</p>
+      <p>Made with 💖 by Akhbarul Hadi (2026)</p>
     </footer>
   );
 }
